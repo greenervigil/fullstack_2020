7dd0c412cfd29e0ef2d8fff39b533e56193f303e
Daniel Vigil <daniel.julian.vigil@gmail.com>
2020-10-15 18:11:25 -0400

    task 1.2

--- part1/courseinformation/src/index.js
+++ part1/courseinformation/src/index.js
@@ -19,16 +19,18 @@ const Header = ({ course }) => {
 const Content = () => {
   return (
     <>
-      <p>
-        {part1} {exercises1}
-      </p>
-      <p>
-        {part2} {exercises2}
-      </p>
-      <p>
-        {part3} {exercises3}
-      </p>
+      <Part part={part1} number={exercises1} />
+      <Part part={part2} number={exercises2} />
+      <Part part={part3} number={exercises3} />
     </>
+  )
+}
+
+const Part = ({ part, number }) => {
+  return (
+    <p>
+        {part} {number}
+    </p>
   )
 }
 
